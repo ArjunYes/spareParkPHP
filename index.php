df4c1832bcd3385647c1b7988e848b3e649242fb
ArjunYes <codingnerd1998@gmail.com>
2022-11-03 13:06:31 +0000

    Added js file

--- index.php
+++ index.php
@@ -5,10 +5,13 @@
     <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="css/all.min.css">
     <link rel="stylesheet" href="css/fontawesome.min.css">
+    <script src="index.js"></script>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Spare Park</title>
+    
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
